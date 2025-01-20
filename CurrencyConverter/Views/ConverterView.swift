49da68e34944aba7ec6c34800ad1cff57c06c7d3
Courage Zeng <czeng@lcs.on.ca>
2025-01-20 10:02:36 -0500

    Initial project organization.

--- CurrencyConverter/Views/ConverterView.swift
+++ CurrencyConverter/Views/ConverterView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ConverterView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    ConverterView()
 }
