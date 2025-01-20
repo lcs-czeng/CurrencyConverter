af29cafbb0aaab9b09a0e6ffbbc49b619f72ba58
Courage Zeng <czeng@lcs.on.ca>
2025-01-20 09:58:23 -0500

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by 曾梓恒 on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
